04018ce652de68a1ba5c5f5dd7a009fb86ce9d3f
Seth Gardner <seth.gardner.a@gmail.com>
2021-08-31 15:59:53 -0500

    finished the servlet exercises except the page counter and the bonuses

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,13 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+@WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
-        int 5;
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+//        res.setContentType("html/text");
+        res.getWriter().println("Hello World");
     }
 }
